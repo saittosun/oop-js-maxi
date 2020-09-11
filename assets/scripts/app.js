fc6c4773aa3e3d8221b735b17882ea4164b0b0de
tostos <saittosun2@gmail.com>
2020-09-11 22:22:07 +0200

    first commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,43 @@
+// jshint esversion: 9
+const productList = {
+  products: [
+    {
+      title: "A Pillow",
+      imageUrl:
+        "https://www.maxpixel.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg",
+      description: "A soft pillow!",
+      price: 19.99,
+    },
+    {
+      title: "A Carpet",
+      imageUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg",
+      description: 'A carpet which you might like - or not.',
+      price: 89.99,
+    },
+  ],
+  render() {
+    const renderHook = document.getElementById("app");
+    const prodList = document.createElement("ul");
+    prodList.className = "product-list";
+    for (const prod of this.products) {
+      const prodEl = document.createElement("li");
+      prodEl.className = "product-item";
+      prodEl.innerHTML = `
+        <div>
+          <img src="${prod.imageUrl}" alt="${prod.title}">
+          <div class="product-item__content">
+            <h2>${prod.title}</h2>
+            <h3>\$${prod.price}</h3>
+            <p>${prod.description}</p>
+            <button>Add to Cart</button>
+          </div>
+        </div>
+      `;
+      prodList.append(prodEl);
+    }
+    renderHook.append(prodList);
+  },
+};
+
+productList.render();
