9fbab91ce1f1fe54823c85ea2e8ffe563f135477
tostos <saittosun2@gmail.com>
2020-09-12 06:19:11 +0200

    using and connecting multiple classes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,11 @@
 // jshint esversion: 9
 class Product {
-  title = "DEFAULT";
-  imageUrl;
-  description;
-  price;
+  // we basically overwrite everything we set up there in the constructor and therefore you can actually remove that. This definition here is not required to be able to assign values, in the constructor you can add properties for the first time, so without having defined fields for them before just like that with the this keyword and indeed it makes no sense or it rarely makes sense to define fields here.
+
+  // title = "DEFAULT";
+  // imageUrl;
+  // description;
+  // price;
 
   //  we need a special method, a method which Javascript automatically calls when we create a new instance of this class and we create a new instance when we call new and then execute this class like a function and this special method which Javascript executes for us is called the constructor method or the constructor function.
   constructor(title, image, desc, price) {
@@ -17,9 +19,32 @@ class Product {
 
 // console.log(new Product());
 
-const productList = {
-  products: [
-    //  New in the end is a keyword Javascript understands that together with such a function execution which is based on a class, it's not a real function but still, should basically create a new object. this returns a new object which has this structure.
+class ProductItem {
+  constructor(product) {
+    // just to re-iterate what we learned: "this.product = product" adds a new 'product' property to the eventually created objects.
+    this.product = product;
+  }
+  render() {
+    const prodEl = document.createElement("li");
+    prodEl.className = "product-item";
+    prodEl.innerHTML = `
+      <div>
+        <img src="${this.product.imageUrl}" alt="${this.product.title}">
+        <div class="product-item__content">
+          <h2>${this.product.title}</h2>
+          <h3>\$${this.product.price}</h3>
+          <p>${this.product.description}</p>
+          <button>Add to Cart</button>
+        </div>
+      </div>
+    `;
+    return prodEl;
+  }
+}
+
+class ProductList {
+  // what will happen here is that when we create an object based on this class, a product's property will be added automatically and the default value will be that array.
+  products = [
     new Product(
       "A Pillow",
       "https://www.maxpixel.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg",
@@ -32,29 +57,24 @@ const productList = {
       "A carpet which you might like - or not.",
       89.99
     ),
-  ],
+  ];
+
+  // the products field is magically added as a property during the construction process anyways.
+  constructor() {}
+
   render() {
     const renderHook = document.getElementById("app");
     const prodList = document.createElement("ul");
     prodList.className = "product-list";
     for (const prod of this.products) {
-      const prodEl = document.createElement("li");
-      prodEl.className = "product-item";
-      prodEl.innerHTML = `
-        <div>
-          <img src="${prod.imageUrl}" alt="${prod.title}">
-          <div class="product-item__content">
-            <h2>${prod.title}</h2>
-            <h3>\$${prod.price}</h3>
-            <p>${prod.description}</p>
-            <button>Add to Cart</button>
-          </div>
-        </div>
-      `;
+      const productItem = new ProductItem(prod);
+      // as you learned, render will return this new object. So now we can append product element again because prod el is such a DOM object created by render
+      const prodEl = productItem.render();
       prodList.append(prodEl);
     }
     renderHook.append(prodList);
-  },
-};
+  }
+}
 
+const productList = new ProductList();
 productList.render();
